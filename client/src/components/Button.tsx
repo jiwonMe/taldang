6f44683fd1ba7ddc1d442002ed861486c8c52359
jiwonMe <park@jiwon.me>
2024-02-18 12:57:23 +0900

    add font

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -14,10 +14,11 @@ const StyledButton = styled.button`
   gap: 10px;
 
   width: 100%;
-  height: 50px;
+  height: 48px;
 
   background: linear-gradient(180deg, #414141 0%, #101010 100%);
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  border: 1px solid #616161;
   border-radius: 8px;
 
   /* Text */
